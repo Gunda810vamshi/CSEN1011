57caad499e86a57a4dc353e99b8797b16b2e2287
Gunda810vamshi <93367756+Gunda810vamshi@users.noreply.github.com>
2021-11-30 14:04:44 +0530

    Update avgmarks.c

--- avgmarks.c
+++ avgmarks.c
@@ -1,3 +1,23 @@
+/*Alogritrhm
+Step 1:Start
+step 2:Enter the marks of a
+Step 3:Enter the marks of b
+step 4:Enter the marks of c
+step 5:Enter the marks of d
+step 6:Enter the marks of e
+Step 7:Calculate average marks by using the formula
+Step 8:Stop
+
+
+
+
+
+
+
+
+
+
+
 #include <stdio.h>
 int main(){
   int a,b,c,d,e;
@@ -21,3 +41,11 @@ printf("Average marks=%d", (a+b+c+d+e)/5);
 
 
 }
+
+
+/*learnings
+........know to excute simple programming
+........know to excute simple programming which includes the formula based.
+/*
+
+
