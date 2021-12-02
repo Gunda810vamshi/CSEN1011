a5ad0a22726f7df8e4f9da51ff3a7b7f58c6185d
Gunda810vamshi <93367756+Gunda810vamshi@users.noreply.github.com>
2021-12-02 14:24:12 +0530

    Update and rename trial.c to switchcase.c

---
+++ switchcase.c
@@ -0,0 +1,34 @@
+/*alogrithm
+step 1:Start
+step 2:take the value
+step 3:excute now
+step 4:stop
+/*
+
+
+#include <stdio.h>
+int main()
+{
+int value;
+scanf("%d",&value);
+switch(value){
+         case 1:printf("value is 1\n");
+
+   break;
+         case 2:printf("value is 2\n");
+
+
+   break;
+          
+          default:printf("Not 1,2");
+
+
+}
+}
+
+
+/*
+learnings
+........know to excute the switch case programming
+.........know the excute the large cases involved programmings by help of switch case method
+/*
