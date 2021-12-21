779eda21b5ebcdbf01fedd71d1584b194f8adcf0
Gunda810vamshi <93367756+Gunda810vamshi@users.noreply.github.com>
2021-12-21 14:38:29 +0530

    Create Floyds triangle.c

---
+++ Floyds triangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d=1,rows;
+	printf("Enter the number of rows ");
+	scanf("%d",&rows);
+	for(a=1;a<=rows;a++){
+		for(b=1;b<=c;b++){
+			printf("%d",d);
+			d+=1;
+		}
+		printf("\n");
+		c+=1;
+	}
+}
