43d9b73acbc0d210d2ff9b8030f4b9a05c1ff483
Gunda810vamshi <93367756+Gunda810vamshi@users.noreply.github.com>
2021-12-28 14:12:22 +0530

    Create swapoftwonumbers.c

---
+++ swapoftwonumbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ void swap(int a,int b){
+     int temp;
+     temp =a;
+     a=b;
+     b=temp;
+    }
+    
+   int main(){
+   
+ int a,b;
+ 
+ printf("value of a= ");
+ scanf("%d",&a);
+ 
+ printf("value of b= ");
+ scanf("%d",&b);
+ 
+ 
+ printf("before swap,value of a:%d \n",a);
+ printf("before swap,value of b:%d \n",b);
+ swap(a,b);
+ printf("after swap,value of a:%d \n",b);
+ printf("after swap,value of b:%d \n",a);
+ }
